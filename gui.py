27b604ea5ac8933b65de0086a017f05c0487845f
nvk23 <nkuznetsov23@gmail.com>
2022-12-10 17:02:39 -0500

    Simple GUI-Added Path to Folder

--- gui.py
+++ gui.py
@@ -5,10 +5,15 @@ from gooey import Gooey
 @Gooey
 def main():
     parser = argparse.ArgumentParser(description='Do stuff with numbers.')
-    parser.add_argument('-o','--output', help="Enter output BIDS directory", type="str", required=False, nargs='+')
-    parser.add_argument('-m','--MEG', help='Enter path of MEG folder', type="str", required=False, nargs='+')
-    parser.add_argument('-n','--NIFTI', help='Enter path of NIFTI folder', type="str", required=False, nargs='+')
-    parser.add_argument('-t','--transformatrix', help='Enter path of transform matrix', type="str", required=False, nargs='+')
+    # parser.add_argument('-o','--output', help="Enter output BIDS directory", type="str", required=False, nargs='+')
+    # parser.add_argument('-m','--MEG', help='Enter path of MEG folder', type="str", required=False, nargs='+')
+    # parser.add_argument('-n','--NIFTI', help='Enter path of NIFTI folder', type="str", required=False, nargs='+')
+    # parser.add_argument('-t','--transformatrix', help='Enter path of transform matrix', type="str", required=False, nargs='+')
+
+    parser.add_argument('-o','--output', help="Enter output BIDS directory", required=False, nargs='+')
+    parser.add_argument('-m','--MEG', help='Enter path of MEG folder', required=False, nargs='+')
+    parser.add_argument('-n','--NIFTI', help='Enter path of NIFTI folder', required=False, nargs='+')
+    parser.add_argument('-t','--transformatrix', help='Enter path of transform matrix', required=False, nargs='+')
 
     args = vars(parser.parse_args())
 
