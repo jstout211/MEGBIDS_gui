4aa5d113ecd1c6fb63bf1ea26da6c5307056c289
nvk23 <nkuznetsov23@gmail.com>
2022-12-10 14:43:28 -0500

    Tested Out QT Designer

--- TEST.py
+++ TEST.py
@@ -28,3 +28,4 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+
